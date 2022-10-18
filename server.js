0f1b9ac9719db813cef4aee300c906baa40b44f0
kawtharnaser <kawtharnasser7@gmail.com>
2022-10-18 06:52:40 +0300

    API is making a request but not correctly...

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const { response } = require('express');
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
@@ -15,10 +16,30 @@ app.use(ejsLayouts);
 // Adds some logging to each request
 app.use(require('morgan')('dev'));
 
+
+let axios = require('axios');
+
 // Routes
+
+//home route
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  console.log(process.env.OMDb_API_KEY)
+  res.render('index')
 });
+
+//results route
+app.get('/results', (req, res)=>{
+  let s = req.body.s
+  console.log(s)
+  // axios.get(`http://www.omdbapi.com/?apikey=${process.env.OMDb_API_KEY}&s=${s}`)
+  axios.get(`http://www.omdbapi.com/?s=${s}=Star+Wars&apikey=${process.env.OMDb_API_KEY}`)
+
+  .then(function (response){
+    res.render('results', {data: response.data.Search})
+  })
+})
+
+
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
